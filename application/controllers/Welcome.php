6aaa30c8a48ffb6cf5c4c7c9742074f158495086
lilianaliray <marcelo_masoka@hotmail.com>
2017-10-19 20:49:55 -0300

    modificacion de funcion
    se crea funcion para recibir los datos

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,9 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller 
+{
 
-	function __construct(){
+	function __construct()
+	{
 		parent::__construct();
 		$this->load->helper('form');
 		$this->load->model('form_model');  
@@ -14,21 +16,13 @@ class Welcome extends CI_Controller {
 		$this->load->view('formulario');
 	}
 	function validar()
-	{	
-		//echo "Hola<br>";
-		//aqui estamos recibiendo los datos del formulario eval.php
-
-		$name = $this->input->post('nombre1');
-		$lastname = $this->input->post('apellido1');
-
-		echo "Nombre: ".$name."  Apellido: ".$lastname;
-		//aqui estamos creando el array
-
-		$data = array('nombre' => $this->input->post('nombre1'),'apellido' => $this->input->post('apellido1'),'Direccion de email'=>$this->input->post('direccion1'),'next'=> $this->input->post('next1') );
-
-//aqui se llama la funcion para guardar en la BD.
-		$this->form_model->guardar($data);
+	{
+		$name=$this->input->post("nombre1");
+		$lastname=$this->input->post("apellido1");
+		$email=$this->input->post("correo1");
+		echo "nombre1:".$name."apellido1:".$lastname."correo1:".$email;
 	}
+
+
 }
-
-
+?>
