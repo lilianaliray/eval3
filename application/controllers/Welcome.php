aeea027f29fae26128d2c76b5f85319270afb3c1
lilianaliray <marcelo_masoka@hotmail.com>
2017-10-19 20:03:07 -0300

    funcion
    crea funcion de datos

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -13,4 +13,22 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('formulario');
 	}
+	function validar()
+	{	
+		//echo "Hola<br>";
+		//aqui estamos recibiendo los datos del formulario eval.php
+
+		$name = $this->input->post('nombre1');
+		$lastname = $this->input->post('apellido1');
+
+		echo "Nombre: ".$name."  Apellido: ".$lastname;
+		//aqui estamos creando el array
+
+		$data = array('nombre' => $this->input->post('nombre1'),'apellido' => $this->input->post('apellido1'),'Direccion de email'=>$this->input->post('direccion de email'),'next'=> $this->input->post('next') );
+
+//aqui se llama la funcion para guardar en la BD.
+		$this->form_model->guardar($data);
+	}
 }
+
+
